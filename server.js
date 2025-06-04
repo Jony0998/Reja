a9e4df338c4afa114a6cea5f4337178704abd6a4
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-06-05 07:18:09 +0900

    feat: EJS Frameworkda partfolio publish completed

--- server.js
+++ server.js
@@ -9,40 +9,39 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
     if(err) {
         console.log("ERROR:", err);
     } else {
-        user = JSON.parse(data)
-    }
-    
-});
-
+        user = JSON.parse(data) 
+    }   
+          
+});           
 // 1: Kirish kodlari
-app.use(express.static("public"));
+app.use(express.static("public"));   
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));   
 
 // 2 : Session code
-
-// 3: Views codes
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4: Routine codes
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    // res.json({test: "success"});
-});
-
-app.get('/author', (req, res) => {
-    res.render("author", {user: user});
-})
-
-app.get("/", function (req, res) { 
-res.render("harid");
-});
-
-
-
-const server = http.createServer(app);
+   
+// 3: Views codes    
+app.set("views", "views");      
+app.set("view engine", "ejs");        
+         
+// 4: Routine codes       
+app.post("/create-item", (req, res) => {      
+    console.log(req.body);   
+    // res.json({test: "success"});       
+});          
+                   
+app.get('/author', (req, res) => {        
+    res.render("author", {user: user});                
+})                      
+     
+app.get("/", function (req, res) {                  
+res.render("harid");         
+});                                    
+                                    
+                       
+            
+const server = http.createServer(app);       
 let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
-});
+});                      
