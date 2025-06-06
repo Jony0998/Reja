8e1329fb43eba9b4d80a8e2f04c8632cfb61316b
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-06-06 21:03:45 +0900

    feat: train callback hamda async

--- train.js
+++ train.js
@@ -1,1 +1,151 @@
-console.log('TRAIN AREA');
+
+function countLetter(taassurotsizlik, word) {
+  let count = 0;
+  
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === taassurotsizlik) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(countLetter("s", "taassurotsizlik")); 
+
+
+// natijamiz 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "Yaxshi talaba bo'ling",  //0-20
+//     "To'g'ri boshliq tanlang va ko'proq xato qiling",  // 20-30
+//     "O'zingizga ishlashni boshlang",   // 30-40
+//     "Siz kuchli bo'lgan narsalarni qiling", // 40-50
+//     "Yoshlarga investititsiya qiling", // 50-60
+//     "Endi dam oling, foydasi yo'q endi" // 60
+// ];
+ 
+// // CALLBACK function
+// function maslahatBering(a, callback) {
+//     if(typeof a !== 'number') callback("insert a number", null);
+//     else if (a <= 20) callback(null,list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4])
+//     else {
+
+//     setInterval(function () {
+//         callback(null, list[5]);
+//     }, 1000);
+//     }
+// }
+
+// console.log("passed here 0");
+// maslahatBering(70, (err, data) => {
+//     if (err) console.log("ERROR:", err);
+//     else {
+//         console.log(data);
+//     }
+// });
+
+// console.log("passed here 1");
+
+
+
+
+
+
+
+
+
+// function maslahatBering(a, callback) {
+//     if(typeof a !== 'number') callback("insert a number", null);
+//     else if (a <= 20) callback(null,list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4])
+//     else {
+
+//     setTimeout(function () {
+//         callback(null, list[5]);
+//     }, 5000);
+//     }
+// }
+
+
+//ASYNC function
+
+//  async function maslahatBering(a,) {
+//     if(typeof a !== 'number') throw new Error("insert a number");
+//     else if (a <= 20) return list[0];
+//     else if (a > 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//     else {
+ 
+//        return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve(list[5]);
+//         }, 5000);
+//        });
+
+//     }
+// }
+
+// call via then/ catch
+// maslahatBering(25)
+// .then((data) => {
+//     console.log("javob:", data);
+// })
+// .catch((err) => {
+//     console.log("ERROR:", err)
+// });
+
+
+// console.log("passed here 1");
+
+
+//call via async/await
+
+// async function run() {
+//     let javob = await maslahatBering(65);  
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+    
+// }
+
+// run();
+
+
+
+
+
+
+
+// maslahatBering(65, (err, data) => {
+//     if(err) console.log('ERROR:', err);
+//     console.log('javob:', data)
+//     });
