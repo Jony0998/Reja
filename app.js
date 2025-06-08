b0735781d3649eb9eedfef59002e48367260d0a7
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-06-08 20:21:51 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -31,18 +31,40 @@ app.set("views", "views");
 app.set("view engine", "ejs");        
          
 // 4: Routine codes       
-app.post("/create-item", (req, res) => {      
-    console.log(req.body);   
-    // res.json({test: "success"});       
+app.post("/create-item", (req, res) => { 
+     console.log('user entered /create-item');  
+
+    console.log(req.body);
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end('something went wrong');
+        } else {
+            res.end('successfully added');
+        }
+    });   
+             
 });          
                    
-app.get('/author', (req, res) => {        
+app.get('/author', (req, res) => { 
+    console.log('user entered /');      
     res.render("author", {user: user});                
 })                      
      
-app.get("/", function (req, res) {                  
-res.render("reja");         
-});                                    
+app.get("/", function (req, res) {  
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.render("reja", {items: data}); 
+        }
+    });                
+          
+});                                     
                                       
   module.exports = app;                     
             
