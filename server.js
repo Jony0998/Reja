16a1599e23a4255dd209cb8b04562c47a45cb732
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-06-08 18:21:42 +0900

    feat: mongoDB connect

--- server.js
+++ server.js
@@ -1,47 +1,33 @@
-console.log('Web Serverni boshlash');
-const express = require("express");
-const app = express();
+
 const http = require("http");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data) 
-    }   
-            
-});           
-// 1: Kirish kodlari  
-app.use(express.static("public"));   
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));   
+const mongodb = require("mongodb");
 
-// 2 : Session code
-   
-// 3: Views codes     
-app.set("views", "views");      
-app.set("view engine", "ejs");        
-         
-// 4: Routine codes       
-app.post("/create-item", (req, res) => {      
-    console.log(req.body);   
-    // res.json({test: "success"});       
-});          
-                   
-app.get('/author', (req, res) => {        
-    res.render("author", {user: user});                
-})                      
-     
-app.get("/", function (req, res) {                  
-res.render("reja");         
-});                                    
-                                      
-                       
-            
-const server = http.createServer(app);       
+
+let db;
+const connectionString = "mongodb+srv://Jony_me:Ov8hUBFhaOpuT8Ry@cluster0.y2l0kvg.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+ }, (err, client) => {
+  if(err) console.log("ERROR on connection MongoDB");
+else {
+    console.log("MongoDB connection succeed");
+   module.exports = client;
+
+
+
+    const app = require("./app");
+   const server = http.createServer(app);       
 let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-});                           
+});  
+
+}
+
+ });
+
+
+
