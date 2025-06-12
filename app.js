3440def5fb28c90ca5099066b350e576695aa6a7
Jony0998 <bozorovjanobiddin9818@gmail.com>
2025-06-12 19:22:47 +0900

    feat: FrontEnd javascriptni qurish

--- app.js
+++ app.js
@@ -32,17 +32,13 @@ app.set("view engine", "ejs");
          
 // 4: Routine codes       
 app.post("/create-item", (req, res) => { 
-     console.log('user entered /create-item');  
+     console.log("user entered /create-item");    
 
     console.log(req.body);
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end('something went wrong');
-        } else {
-            res.end('successfully added');
-        }
+        console.log(data.ops);
+       res.json(data.ops[0]);
     });   
              
 });          
@@ -52,7 +48,7 @@ app.get('/author', (req, res) => {
     res.render("author", {user: user});                
 })                      
      
-app.get("/", function (req, res) {  
+app.get("/", function (req, res) {   
     db.collection("plans")
     .find()
     .toArray((err, data) => {
